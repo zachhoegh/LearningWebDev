47858cd21b1770a554f300bede03a342fbcb0585
zachhoegh <zachhoegh@gmail.com>
2026-01-07 12:09:53 -0600

    hello world button

--- LearningWebDev/Program.cs
+++ LearningWebDev/Program.cs
@@ -1,6 +1,10 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", async (HttpContext context) =>
+{
+    var html = await File.ReadAllTextAsync("wwwroot/index.html");
+    return Results.Content(html, "text/html");
+});
 
 app.Run();
